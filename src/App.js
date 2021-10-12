b5f90a591ce49c143578ad67bda649e8aadb056b
dendcode <dendcode@gmail.com>
2021-10-12 18:53:07 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Website is Run on AWS
+      <header className="App-header">        
+        <p style={{fontSize: 66, padding: 0, margin: 0}}>
+          DendCode 네트워크에
         </p>
-      </header>
+        <p style={{fontSize: 66, padding: 0, margin: 0, marginBottom: 50}}>
+        접속하신 걸 환영합니다
+        </p>
+        <AmplifySignOut />
+      </header>      
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
